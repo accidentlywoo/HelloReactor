dd7efd7a8d712787e51d94b3b9bb616b18c9521d
accidentlywoo <accidentlywoo@gmail.com>
2021-08-09 00:26:25 +0900

    Flux에서 스케줄러를 사용하는 코드를 뚝딱만들어보기

---
+++ src/main/java/reactor/study/reactor/reactivestream/FluxScEx.java
@@ -0,0 +1,17 @@
+package reactor.study.reactor.reactivestream;
+
+import reactor.core.publisher.Flux;
+import reactor.core.scheduler.Schedulers;
+
+public class FluxScEx {
+	public static void main(String[] args) {
+		Flux.range(1, 10)
+				.publishOn(Schedulers.newSingle("pub"))
+				.log()
+				.subscribeOn(Schedulers.newSingle("sub"))
+				.subscribe(System.out::println)
+		;
+
+		System.out.println("exit");
+	}
+}
